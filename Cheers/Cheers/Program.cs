5e7311dbd8238652c571505b453494d9c3509bf1
Richard 8s <rick8s@hotmail.com>
2015-10-14 12:27:50 -0500

    First commit with cheer

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello there! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+            foreach (char letter in name)
+                {
+                string choose = "a";
+                foreach(char annies in "aefhilmnorsxAEFHILMNORSX")
+                    {
+                        if (letter == annies)
+                        {
+                            choose = "an";
+                        }   
+                    }
+                System.Console.WriteLine("Give me " + choose + " " + letter);
+                }
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
