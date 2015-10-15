c8f9789146ee30aab73c09b454c03ddbf0bf7cbf
Richard 8s <rick8s@hotmail.com>
2015-10-14 21:46:42 -0500

    Added birthday calculation functionality

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -12,7 +12,13 @@ namespace Cheers
         {
             System.Console.WriteLine("Hello there! What's your name?");
             string name = System.Console.ReadLine();
-            System.Console.WriteLine("Hi, " + name);
+            System.Console.WriteLine("Hi, " + name + ", " + "What's your birthday? (DD/MM)");
+            DateTime start = DateTime.Now;
+            string birthday = System.Console.ReadLine();
+            DateTime end = DateTime.Parse(birthday);
+            int difference = (start - end).Days;
+            System.Console.WriteLine("start: " + start + " end: "+ end+ "  difference: " + difference);
+
             foreach (char letter in name)
                 {
                 string choose = "a";
@@ -26,7 +32,22 @@ namespace Cheers
                 System.Console.WriteLine("Give me " + choose + " " + letter);
                 }
             name = name.ToUpper();
-            System.Console.WriteLine(name + "'s just GRAND!");
+            System.Console.WriteLine(name + "'s just GRAND!" + "\n");
+            if (difference < 0)
+            { 
+                int remainingDays = 0 - difference;
+                System.Console.WriteLine("Your birthday is " + remainingDays + " days away!");   
+            }
+            else if (difference == 0)
+ 
+                {
+                    System.Console.WriteLine("Happy Birthday!!");
+                }
+            else
+            {
+                int remainingDays = 365 - (difference);
+                System.Console.WriteLine("I missed your birthday this year.\n" + "Now it's " + remainingDays + " days away!\n" + "Happy belated birthday anyway.");
+            }
             System.Console.WriteLine("Press any key to exit");
             System.Console.ReadKey();
         }
